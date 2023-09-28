d30da54f085103628fa0afd96d3092a9c7cfb255
lovehifi <81028393+lovehifi@users.noreply.github.com>
2023-09-28 12:54:20 +0700

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
-
 DOCKER_GPG_FILE="/usr/share/keyrings/docker-archive-keyring.gpg"
 INSTALLING=1
-
-
 OPT_DIR="/usr/local/docker"
-
-
 check_root() {
     if [[ $EUID -ne 0 ]]; then
         echo "This script must be run as root." 
         exit 1
     fi
 }
-
-
 check_root
-
 echo "Installing Docker..."
 sudo apt-get update
 sudo apt-get install -y \
@@ -25,17 +17,15 @@ sudo apt-get install -y \
     gnupg \
     lsb-release
 
-
 if [ -f "${DOCKER_GPG_FILE}" ]; then
     rm "${DOCKER_GPG_FILE}"
 fi
-
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o "${DOCKER_GPG_FILE}"
 echo "deb [arch=$(dpkg --print-architecture) signed-by=${DOCKER_GPG_FILE}] https://download.docker.com/linux/debian \
     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+
 
 echo "Docker installed."
 
