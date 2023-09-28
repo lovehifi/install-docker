9838b90bd8ed05497cefd9fcead21b08c2afd99a
lovehifi <81028393+lovehifi@users.noreply.github.com>
2023-09-28 12:50:06 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,22 @@
-#! /bin/bash
+#!/bin/bash
 
 DOCKER_GPG_FILE="/usr/share/keyrings/docker-archive-keyring.gpg"
 INSTALLING=1
 
-[ -z "${OPT_DIR}" ]
+
+OPT_DIR="/usr/local/docker"
+
+
+check_root() {
+    if [[ $EUID -ne 0 ]]; then
+        echo "This script must be run as root." 
+        exit 1
+    fi
+}
+
+
 check_root
+
 echo "Installing Docker..."
 sudo apt-get update
 sudo apt-get install -y \
@@ -12,15 +24,20 @@ sudo apt-get install -y \
     curl \
     gnupg \
     lsb-release
+
+
 if [ -f "${DOCKER_GPG_FILE}" ]; then
-  rm "${DOCKER_GPG_FILE}"
+    rm "${DOCKER_GPG_FILE}"
 fi
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg |  gpg --dearmor -o "${DOCKER_GPG_FILE}"
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=${DOCKER_GPG_FILE}] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" |  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o "${DOCKER_GPG_FILE}"
+echo "deb [arch=$(dpkg --print-architecture) signed-by=${DOCKER_GPG_FILE}] https://download.docker.com/linux/debian \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+
 echo "Docker installed."
+
 
 #wget -O - https://raw.githubusercontent.com/lovehifi/install-docker/main/install.sh | sh
